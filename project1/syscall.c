6d9c5a54015baedb32f298baec67c580cd45fa27
Al Manrique <56333028+agm08@users.noreply.github.com>
2020-02-09 23:16:13 -0500

    Create syscall.c

---
+++ project1/syscall.c
@@ -0,0 +1,44 @@
+/* syscall.c
+ *
+ * Group Members Names and NetIDs:
+ *   1. Al Manrique am2240
+ *   2. Alex P.
+ *
+ * ILab Machine Tested on:
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>                                                                                
+#include <sys/syscall.h>
+
+
+double avg_time = 0;
+
+int main(int argc, char *argv[]) {
+	
+    /* Implement Code Here */
+	
+	int i;
+	double time = 0;
+	struct timeval start;
+	struct timeval end;
+	
+	for(i = 0; i<5000000; i++){
+	
+		gettimeofday(&start, 0);
+		syscall(getuid());
+		gettimeofday(&end, 0);
+		time += ((end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec-start.tv_usec));
+	}
+	avg_time = time/5000000;
+	
+	
+    // Remember to place your final calculated average time
+    // per system call into the avg_time variable
+	
+    printf("Average time per system call is %0.4f microseconds\n", avg_time);
+
+    return 0;
+}
